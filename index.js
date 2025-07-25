2cf00daac5f1e43784ace8adfca35f4090bc6dd5
krzysztofe <gitaragra@gmai.com>
2025-07-25 20:58:05 +0200

    Versio2 in vercel.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const startServer = async () => {
   try {
     // await mongoose.connect(process.env.MONGO_CONNECTION);
     console.log("Connected to MongoDB");
-    app.listen("https://godziny-server.vercel.app", () => {
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (err) {
